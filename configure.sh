1fad462f9ec75395422ce44bcf8652a11f705aff
David Heryanto <david.heryanto@hotmail.com>
2015-12-15 03:30:28 +0800

    Add system-config-printer install

--- configure.sh
+++ configure.sh
@@ -1,5 +1,5 @@
 # Startup config after fresh install Fedora
-sudo dnf -y install gcc gcc-c++ gcc-gfortran vim htop gnome-tweak-tool kdiff3 git nethogs mlocate unar rsync wine
+sudo dnf -y install gcc gcc-c++ gcc-gfortran vim htop gnome-tweak-tool kdiff3 git nethogs mlocate unar rsync wine system-config-printer
 
 sudo dnf -y install http://www.infinality.net/fedora/linux/20/noarch/fontconfig-infinality-1-20130104_1.noarch.rpm http://www.infinality.net/fedora/linux/20/x86_64/freetype-infinality-2.4.12-1.20130514_01.fc18.x86_64.rpm; echo 4 | sudo /etc/fonts/infinality/infctl.sh setstyle
 
