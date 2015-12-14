ad1caf869d368b053fba589b783de764be1bc2db
David Heryanto <david.heryanto@hotmail.com>
2015-12-15 01:05:25 +0800

    Remove gnome part

--- configure.sh
+++ configure.sh
@@ -3,8 +3,6 @@ sudo dnf -y install gcc gcc-c++ vim htop gnome-tweak-tool kdiff3 git nethogs mlo
 
 sudo dnf -y install http://www.infinality.net/fedora/linux/20/noarch/fontconfig-infinality-1-20130104_1.noarch.rpm http://www.infinality.net/fedora/linux/20/x86_64/freetype-infinality-2.4.12-1.20130514_01.fc18.x86_64.rpm; echo 4 | sudo /etc/fonts/infinality/infctl.sh setstyle
 
-gsettings set org.gnome.software download-updates false
-
 git config --global user.name "David Heryanto"; git config --global user.email david.heryanto@hotmail.com
 git config --global push.default simple
 git config --global merge.tool kdiff3
