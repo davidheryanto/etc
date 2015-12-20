dc0ddd6e06be32cdc6c28bba71c504c35169f55f
David Heryanto <david.heryanto@hotmail.com>
2015-12-20 16:24:06 +0800

    Add cmake

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 sudo sh -c 'printf "\n%%wheel        ALL=(ALL)       NOPASSWD: ALL  # sudo no passwd\n" >> /etc/sudoers'
 
 # Startup config after fresh install Fedora
-sudo dnf -y install gcc wget gcc-c++ gcc-gfortran vim htop gnome-tweak-tool kdiff3 git nethogs mlocate unar rsync wine system-config-printer python-devel
+sudo dnf -y install gcc wget gcc-c++ gcc-gfortran vim htop gnome-tweak-tool kdiff3 git nethogs mlocate unar rsync wine system-config-printer python-devel cmake
 
 sudo dnf -y install http://www.infinality.net/fedora/linux/20/noarch/fontconfig-infinality-1-20130104_1.noarch.rpm http://www.infinality.net/fedora/linux/20/x86_64/freetype-infinality-2.4.12-1.20130514_01.fc18.x86_64.rpm; echo 4 | sudo /etc/fonts/infinality/infctl.sh setstyle
 
