61ea3cbadd7120808e30ec6389b60b5e5a33eb2a
David Heryanto <david.heryanto@hotmail.com>
2016-01-23 12:53:15 +0800

    Add configure-fedora

---
+++ configure-fedora.sh
@@ -0,0 +1,6 @@
+sudo dnf -y install vim gcc gnome-tweak-tool htop http://www.infinality.net/fedora/linux/20/noarch/fontconfig-infinality-1-20130104_1.noarch.rpm http://www.infinality.net/fedora/linux/20/x86_64/freetype-infinality-2.4.12-1.20130514_01.fc18.x86_64.rpm
+git config --global user.name "David Heryanto"; git config --global user.email david.heryanto@hotmail.com
+git config --global push.default simple
+git config --global merge.tool kdiff3
+
+sudo updatedb
