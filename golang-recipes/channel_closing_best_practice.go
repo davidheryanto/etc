d88f4697bbcc46af7286aac62354a9ac4b13f101
David Heryanto <david.heryanto@hotmail.com>
2019-06-16 22:05:22 +0800

    Update golang

---
+++ golang-recipes/channel_closing_best_practice.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+const timeoutInSec = 3
+
+// Ref: https://github.com/Masterminds/go-in-practice/tree/master/chapter3
+// Example of properly closing a channel
+// "Sender" should be the one "closing" the channel
+func main() {
+	msg := make(chan string)
+	done := make(chan bool)
+	until := time.After(timeoutInSec * time.Second)
+
+	go send(msg, done)
+
+	for {
+		select {
+		case m := <-msg:
+			fmt.Println(m)
+		case <-until:
+			done <- true
+			time.Sleep(500 * time.Millisecond)
+			return
+		}
+	}
+}
+
+// "ch" is a receiving channel, "done" is a sending channel
+func send(ch chan<- string, done <-chan bool) {
+	for {
+		select {
+		case <-done:
+			println("Done")
+			close(ch)
+			return
+		default:
+			ch <- "hello"
+			time.Sleep(500 * time.Millisecond)
+		}
+	}
+}
