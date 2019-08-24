0e0dd0e6e751896828d3b8c8dd652b121ba916b0
David Heryanto <david.heryanto@hotmail.com>
2019-08-24 17:46:11 +0800

    Update example of KafkaCommitOffset

--- kafka-recipes/ExampleKafkaCommitOffset.java
+++ kafka-recipes/ExampleKafkaCommitOffset.java
@@ -1,43 +1,64 @@
-// First check if there is an existing consumer group
-String consumerGroupId = "consumerGroupIdToCheck";
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.ListConsumerGroupOffsetsOptions;
+import org.apache.kafka.clients.admin.ListConsumerGroupOffsetsResult;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.TopicPartition;
 
-Properties adminProperties = new Properties();
-adminProperties.put(
-  CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG,
-  "localhost:9092"));
-AdminClient adminClient = AdminClient.create(adminProperties);
-ListConsumerGroupOffsetsResult listConsumerGroupOffsetsResult =
-adminClient.listConsumerGroupOffsets(
-  consumerGroupId, new ListConsumerGroupOffsetsOptions().timeoutMs(5000));
-Map<TopicPartition, OffsetAndMetadata> topicPartitionOffsetAndMetadataMap =
-listConsumerGroupOffsetsResult.partitionsToOffsetAndMetadata().get(5, TimeUnit.SECONDS);
-boolean consumerGroupExists = topicPartitionOffsetAndMetadataMap.size() > 0;
+public class ExampleKafkaCommitOffset {
+  public static void main(String[] args)
+      throws InterruptedException, ExecutionException, TimeoutException {
+    // First check if there is an existing consumer group
+    final String CONSUMER_GROUP_ID = "myconsumergroup";
+    final String TOPICS = "test1,test2";
+    final String BOOTSTRAP_SERVERS = "localhost:9092";
 
-if (!consumerGroupExists) {
-  log.info(
-    "Consumer group '{}' does not exist, will create it and commit the latest offset",
-    consumerGroupId);
-  Properties consumerProperties = new Properties();
-  consumerProperties.setProperty(
-    "bootstrap.servers", "localhost:9092");
-  consumerProperties.setProperty("group.id", consumerGroupId);
-  consumerProperties.setProperty(
-    "key.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
-  consumerProperties.setProperty(
-    "value.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
-  KafkaConsumer<String, String> consumer = new KafkaConsumer<>(consumerProperties);
+    Properties adminProperties = new Properties();
+    adminProperties.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+    AdminClient adminClient = AdminClient.create(adminProperties);
+    ListConsumerGroupOffsetsResult listConsumerGroupOffsetsResult =
+        adminClient.listConsumerGroupOffsets(
+            CONSUMER_GROUP_ID, new ListConsumerGroupOffsetsOptions().timeoutMs(5000));
+    Map<TopicPartition, OffsetAndMetadata> topicPartitionOffsetAndMetadataMap =
+        listConsumerGroupOffsetsResult.partitionsToOffsetAndMetadata().get(5, TimeUnit.SECONDS);
+    boolean consumerGroupExists = topicPartitionOffsetAndMetadataMap.size() > 0;
 
-  for (String topic : sourceSpec.getOptionsOrThrow("topics").split(",")) {
-    topic = topic.trim();
-    List<PartitionInfo> partitionInfos =
-    consumer.partitionsFor(topic, Duration.ofSeconds(10));
-    List<TopicPartition> topicPartitions = new ArrayList<>();
-    for (PartitionInfo partitionInfo : partitionInfos) {
-      topicPartitions.add(
-        new TopicPartition(partitionInfo.topic(), partitionInfo.partition()));
+    if (!consumerGroupExists) {
+      System.out.println(
+          String.format(
+              "Consumer group '%s' does not exist, will create it and commit the latest offset",
+              CONSUMER_GROUP_ID));
+
+      Properties consumerProperties = new Properties();
+      consumerProperties.setProperty("bootstrap.servers", BOOTSTRAP_SERVERS);
+      consumerProperties.setProperty("group.id", CONSUMER_GROUP_ID);
+      consumerProperties.setProperty(
+          "key.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
+      consumerProperties.setProperty(
+          "value.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
+      KafkaConsumer<String, String> consumer = new KafkaConsumer<>(consumerProperties);
+
+      for (String topic : TOPICS.split(",")) {
+        topic = topic.trim();
+        List<PartitionInfo> partitionInfos = consumer.partitionsFor(topic, Duration.ofSeconds(10));
+        List<TopicPartition> topicPartitions = new ArrayList<>();
+        for (PartitionInfo partitionInfo : partitionInfos) {
+          topicPartitions.add(new TopicPartition(partitionInfo.topic(), partitionInfo.partition()));
+        }
+        consumer.assign(topicPartitions);
+        consumer.poll(1000);
+        consumer.commitSync();
+      }
     }
-    consumer.assign(topicPartitions);
-    consumer.poll(1000);
-    consumer.commitSync();
   }
 }
