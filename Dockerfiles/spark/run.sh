ca1e8b20038ee84bf89e48696b71db3fd1369f60
David Heryanto <david.heryanto@hotmail.com>
2019-12-11 02:02:38 +0800

    Update running spark

--- Dockerfiles/spark/run.sh
+++ Dockerfiles/spark/run.sh
@@ -25,3 +25,23 @@ docker run --rm -it --net host \
   -e SPARK_LOCAL_STORAGE_ENCRYPTION_ENABLED=no \
   -e SPARK_SSL_ENABLED=no \
   bitnami/spark:2
+
+# Download Spark 2.4.4 release
+DOWNLOAD_URL=https://github.com/apache/spark/archive/v2.4.4.tar.gz
+wget -O- $DOWNLOAD_URL | tar -C ~/Downloads -xz
+sudo mv ~/Downloads/spark-2.4.4 /opt/spark-2.4.4
+cat <<EOF >> ~/.bashrc
+# Spark 2.4.4
+export PATH=\$PATH:/opt/spark-2.4.4/bin
+EOF
+
+# Submitting applications
+spark-submit \
+  --master spark://localhost:7070 \
+  --deploy-mode client \
+  main.py
+
+# Setup Yarn
+DOWNLOAD_URL=https://www-eu.apache.org/dist/hadoop/common/hadoop-2.9.2/hadoop-2.9.2.tar.gz
+wget -O- $DOWNLOAD_URL | tar -C ~/Downloads -xz
+cd hadoop-rel-release-2.10.0/
