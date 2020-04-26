c178f31ec1972a0ff65076de903e8d6078087ae1
David Heryanto <david.heryanto@hotmail.com>
2020-04-26 17:12:58 +0800

    Add sample haproxy.cfg

--- ambassador/grpc-example/greeter_client.py
+++ ambassador/grpc-example/greeter_client.py
@@ -14,6 +14,8 @@
 """The Python implementation of the GRPC helloworld.Greeter client."""
 
 from __future__ import print_function
+
+import argparse
 import logging
 
 import grpc
@@ -22,16 +24,19 @@ import helloworld_pb2
 import helloworld_pb2_grpc
 
 
-def run():
+def run(address):
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
-    with grpc.insecure_channel('localhost:50051') as channel:
+    with grpc.insecure_channel(address) as channel:
         stub = helloworld_pb2_grpc.GreeterStub(channel)
-        response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+        response = stub.SayHello(helloworld_pb2.HelloRequest(name="you"))
     print("Greeter client received: " + response.message)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--address", type=str, default="localhost:50051")
+    args = parser.parse_args()
     logging.basicConfig()
-    run()
+    run(args.address)
