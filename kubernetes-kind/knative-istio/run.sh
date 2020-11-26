858031d775ecd8091b67193242245ae9f436a916
David Heryanto <david.heryanto@hotmail.com>
2020-11-26 08:29:17 +0800

    Update kind knative-istio example with local registry

--- kubernetes-kind/knative-istio/run.sh
+++ kubernetes-kind/knative-istio/run.sh
@@ -1,16 +1,20 @@
-kind create cluster --config cluster.yaml --wait 120s
+./kind-with-registry.sh
 
 # Install Serving component
-kubectl apply --filename https://github.com/knative/serving/releases/download/v0.17.0/serving-crds.yaml
-kubectl apply --filename https://github.com/knative/serving/releases/download/v0.17.0/serving-core.yaml
+kubectl apply --filename https://github.com/knative/serving/releases/download/v0.19.0/serving-crds.yaml
+kubectl apply --filename https://github.com/knative/serving/releases/download/v0.19.0/serving-core.yaml
 
 # Install Istio for Knative. Make sure "istioctl" is installed.
 istioctl install -f istio-minimal-operator.yaml
-kubectl apply --filename https://github.com/knative/net-istio/releases/download/v0.17.0/release.yaml
+kubectl apply --filename https://github.com/knative/net-istio/releases/download/v0.19.0/release.yaml
 kubectl patch configmap/config-domain \
   --namespace knative-serving \
   --type merge \
   --patch '{"data":{"127.0.0.1.nip.io":""}}'
+kubectl patch configmap/config-deployment \
+  --namespace knative-serving \
+  --type merge \
+  --patch  '{"data":{"registriesSkippingTagResolving": "localhost:5000"}}'
 
 # Test the installation
 kubectl apply -f foobar.yaml
