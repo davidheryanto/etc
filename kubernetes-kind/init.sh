d3a2d6ca26cc9d89cc152a5096b25f4e378d66c8
David Heryanto <david.heryanto@hotmail.com>
2020-12-07 18:00:37 +0800

    Install metrics server in Kind Kube cluster

--- kubernetes-kind/init.sh
+++ kubernetes-kind/init.sh
@@ -27,3 +27,7 @@ nodes:
 - role: worker
 - role: worker
 EOF
+
+# Install metrics server: https://github.com/kubernetes-sigs/kind/issues/398
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.6/components.yaml
+kubectl patch deployment metrics-server -n kube-system -p '{"spec":{"template":{"spec":{"containers":[{"name":"metrics-server","args":["--cert-dir=/tmp", "--secure-port=4443", "--kubelet-insecure-tls","--kubelet-preferred-address-types=InternalIP"]}]}}}}'
